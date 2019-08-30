69514c518d313e003b3a21def641a74e5698a88a
Patrick Bandini <pbandini@vista.it>
2019-08-30 12:09:56 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# dvw_suite
---
+++ src/UI/main.java
@@ -0,0 +1,22 @@
+package UI;
+
+import javax.swing.*;
+
+public class main {
+	private static void createAndShowGUI() {
+        JFrame frame = new JFrame("HelloWorldSwing");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        JLabel label = new JLabel("Hello World");
+        frame.getContentPane().add(label);
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            public void run() {
+                createAndShowGUI();
+            }
+        });
+    }
+}
